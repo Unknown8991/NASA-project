7541d44b886c66f9f1ac6ebc7ba0966c8f4f804f
Unknown8991 <rafal.pat@onet.pl>
2020-10-15 20:11:39 +0200

    Create basic App view

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Hello  asdasdsa sss</h1>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'About',
+};
+</script>
